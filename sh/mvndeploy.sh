9348db89148b4af724479fc0950b8a7492a0472d
stanislawbartkowski@gmail.com <leszek123>
2020-03-10 21:17:51 +0000

    change in pom.xml, package

--- sh/mvndeploy.sh
+++ sh/mvndeploy.sh
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile=../out/artifacts/RestService_jar/RestService.jar  -DgroupId=com.restservice  -DartifactId=RestService  -Dpackaging=jar -Dversion=1.0
+mvn install:install-file -Dfile=../target/RestService-1.0-SNAPSHOT.jar  -DgroupId=com.restservice  -DartifactId=RestService  -Dpackaging=jar -Dversion=1.0
