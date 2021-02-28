d236fa6d4a2fffa2ffd163c4e9523809eb9a2352
sbartkowski <stanislawbartkowski@gmail.com>
2021-02-28 21:54:05 +0100

    Add int type

--- mvndeploy.sh
+++ mvndeploy.sh
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile=target/RestService-1.0-SNAPSHOT.jar  -DgroupId=com.restservice  -DartifactId=RestService  -Dpackaging=jar -Dversion=1.0
+mvn install:install-file -Dfile=target/restservice-1.0.jar  -DgroupId=com.restservice  -DartifactId=restservice  -Dpackaging=jar -Dversion=1.0
